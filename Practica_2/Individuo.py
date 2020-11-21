fe77ee28d33b2cecd92516e45ccf70d08f3b377e
juanM20 <jmtp12poke@gmail.com>
2020-11-21 02:34:00 -0600

    Clase individuo y primer tipo de selección

--- Practica_2/Individuo.py
+++ Practica_2/Individuo.py
@@ -1,9 +1,19 @@
 class Individuo:
 
-    def __init__(self, genotipo, fenotipo):
+    def __init__(self, genotipo = [], fenotipo=0,aptitud = 0 ,apto_cruza = False, prob = 0.0):
         self.genotipo = genotipo
         self.fenotipo = fenotipo
+        self.aptitud = aptitud
+        self.apto_cruza = apto_cruza
+        self.prob = prob
 
 
-    def Crear_fenotipo(genotipo):
+    def Generar_fenotipo(self):
+
+        str_bin = "".join(map(str, self.genotipo))
+        self.fenotipo = int(str_bin,2)
+
+
+
+
         
--- Practica_2/seleccion_1.py
+++ Practica_2/seleccion_1.py
@@ -1,23 +1,62 @@
+from Individuo import Individuo
+import math
+import random
+
+def funcion2(num):
+    return abs((num-5)/(2+math.sin(num)))
 
 
-def seleccion_1(individuos):
+def seleccion_1(poblacion):
 
     seleccion = []
+    rango_prob = []
+    sum = 0
+
+    for ind in poblacion:
+        ind.aptitud = funcion2(ind.fenotipo)
+        sum = sum + ind.aptitud
+
+    for ind in poblacion:
+        ind.prob = ind.aptitud / sum
+
+    rango_prob.append([0,poblacion[0].prob])
+    rango_prob.append([poblacion[0].prob, poblacion[0].prob + poblacion[1].prob])
+
+    
+    for i in range(len(poblacion)):
+        va = random.uniform(0,1)
+
+        if va > rango_prob[i][0] and va < rango_prob[i][1]:
+            poblacion[i].apto_cruza = True
+            seleccion.append(poblacion[i])
+        else:
+            poblacion[i].apto_cruza = False
+            seleccion.append(poblacion[i])
+
 
     return seleccion
 
 
 
-
-
-
 if __name__ == '__main__':
     
-    individuos = [
-        [1,0,1,1],
-        [1,0,0,1],
-        [0,0,1,0],
-        [1,0,0,0]
-    ]
+    poblacion = []
+    Ind1 = Individuo([1,0,0,1,1])
+    Ind1.Generar_fenotipo()
+    Ind2 = Individuo([0,1,1,1,0])
+    Ind2.Generar_fenotipo()
+    
+
+    poblacion.append(Ind1)
+    poblacion.append(Ind2)
+
+    select = seleccion_1(poblacion)
+
+    for i in select:
+        print(i.genotipo, i.apto_cruza)
+
+   
+
+
 
     
