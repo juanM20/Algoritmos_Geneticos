a935339f1c739c5a81cb01045b70575345ecddcd
Quesa <johndoe@example.com>
2020-11-24 13:48:28 -0600

    Graficador

--- Practica_2/Funciones_math.py
+++ Practica_2/Funciones_math.py
@@ -1,4 +1,6 @@
 import math
+import matplotlib.pyplot as plt
+from Individuo import * 
 
 def funcion1(num):
     return num**2
@@ -8,3 +10,14 @@ def funcion2(num):
 
 def funcion3(num):
     return ((math.exp(num) - math.exp(-num))/num)
+
+def graficar(poblacion):
+    arreglo_y = [ind.aptitud for ind in poblacion]
+    arreglo_x = [i for i in range(1,len(poblacion)+1)]
+
+    print(len(poblacion))
+    print(poblacion)
+
+    plt.stem(arreglo_x, arreglo_y)
+    plt.show()
+    return()
