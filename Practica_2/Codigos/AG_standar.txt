893f7fed7884cc5eb91fc74ee8ca26ff0d2f6d9c
Quesa <johndoe@example.com>
2020-12-14 00:07:39 -0600

    Cruza uniforme

---
+++ Practica_2/Codigos/AG_standar.txt
@@ -0,0 +1,118 @@
+from Individuo import Individuo
+from Funciones_seleccion import *
+from Funciones_Cruza import *
+from Funciones_Mutacion import *
+from Funciones_math import *
+
+
+def Generar_poblacion(num_poblacion, num_alelos, opc):
+
+    cromosoma = []
+    poblacion = []
+
+    for i in range(1, (num_poblacion*num_alelos)+1):
+
+        if i % num_alelos == 0:
+
+            if random.random() > 0.5:
+                cromosoma.append(1)
+            else:
+                cromosoma.append(0)
+
+            ind = Individuo(cromosoma)
+            ind.Generar_fenotipo()
+            ind.Generar_Aptitud(opc)
+            poblacion.append(ind)
+            cromosoma = []
+        else:
+            if random.random() > 0.5:
+                cromosoma.append(1)
+            else:
+                cromosoma.append(0)
+
+    return poblacion
+
+
+if __name__ == "__main__":
+
+    opc1 = int(input('''
+
+            Selecciona la función (escribe número).
+
+            1. f(x) = x^2
+            2. f(X) = |(x-5)/2+sin(x)|
+            3. f(x) = (e^x - e^-x)
+    '''))
+
+    num_poblacion = 2
+    num_generacion = int(input("Numero de Generaciones \n"))
+    num_alelos = int(input("Tamaño de los alelos \n"))
+
+    poblacion = []
+    poblacion = Generar_poblacion(num_poblacion, num_alelos, opc1)
+    
+    for ind in poblacion:
+        print(ind.genotipo)
+        print(ind.fenotipo)
+
+    for i in range(num_generacion):
+
+        print(f'''
+              
+              -------------------
+                GENERACION {i+1}
+              -------------------
+              
+              ''')
+          
+        seleccion_cruza = []
+        Hijos = []
+        
+        seleccion_cruza = seleccion_1(poblacion)
+
+        if seleccion_cruza[0].apto_cruza == True or seleccion_cruza[1].apto_cruza == True:
+
+            Hijos = Cruza_Punto(poblacion)
+
+            print("Hijos de generacion: ",i+1)
+            for ind in Hijos:
+                print(ind.genotipo)
+                print(ind.fenotipo)
+
+            r = random.randint(0, 1)
+
+            opc = int(input('''
+                            1. Mutacion aleatoria
+                            2. Mutacion intercambio de bit
+                            Elige una opción:
+                    '''))
+            if opc == 1:
+                mutacion_desplazamiento(seleccion_cruza[r])
+            else:
+                CMintercambio(seleccion_cruza[r])
+
+            poblacion = seleccion_cruza.copy()
+        
+        else: 
+            r = random.randint(0, 1)
+
+            opc = int(input('''
+                            1. Mutacion aleatoria
+                            2. Mutacion intercambio de bit
+                            Elige una opción:
+                    '''))
+            if opc == 1:
+                mutacion_desplazamiento(seleccion_cruza[r])
+            else:
+                CMintercambio(seleccion_cruza[r])
+
+            poblacion = seleccion_cruza.copy()
+
+            
+        print("Nueva poblacion: ")
+        for ind in poblacion:
+            ind.Generar_Aptitud(opc1)
+            print(ind.genotipo)
+            print(ind.fenotipo)
+
+        graficar(poblacion)
--- Practica_2/Funciones_Mutacion.py
+++ Practica_2/Funciones_Mutacion.py
@@ -252,7 +252,104 @@ def L4_(v1, p1, p2, p3, unos):
 
 #comb = permutations(v1, len(v1)) 
 #for i in comb:
-#	print(i)
+#print(i)
+def mutacion_heuristica(v1):
+	v1 = np.array([0, 0, 0, 1, 1, 0])
+	L = random.randint(len(v1)-2) #Lambda=Numero de alhelos a cambiar de lugar
+	if(L==0): #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		print("No hay mutacion: ")
+		print("Sin cambios: ", v1)
+	elif(L==1): #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		print("No hay mutacion: ")
+		print("Sin cambios: ", v1)
+	elif(L==2): #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		while True:
+			p1 = random.randint(len(v1))
+			p2 = random.randint(len(v1))
+			if (p1!=p2):
+				break
+		print("Antes: ", v1)
+		print("Punto 1: ", p1)
+		print("Punto 2: ", p2)
+
+		if(v1[p1]==0 and v1[p2]==0): #00
+			print("Despues: ", v1)
+		elif(v1[p1]==1 and v1[p2]==1): #11
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==0) or (v1[p1]==0 and v1[p2]==1)): #10
+			if(p1 < p2):
+				v1[p1] = 1
+				v1[p2] = 0
+			elif(p2 < p1):
+				v1[p2] = 1
+				v1[p1] = 0
+			print("Despues: ", v1)
+	elif(L==3): #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		while True:
+			p1 = random.randint(len(v1))
+			p2 = random.randint(len(v1))
+			p3 = random.randint(len(v1))
+			if ((p1!=p2) and (p1!=p3) and (p2!=p3)):
+				break
+		print("Antes: ", v1)
+		print("Punto 1: ", p1)
+		print("Punto 2: ", p2)
+		print("Punto 3: ", p3)
+	 
+		if(v1[p1]==0 and v1[p2]==0 and v1[p3]==0): #000
+			print("Despues: ", v1)
+		elif(v1[p1]==1 and v1[p2]==1 and v1[p3]==1): #111
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==0 and v1[p3]==0) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==0) or \
+			(v1[p1]==0 and v1[p2]==0 and v1[p3]==1)): #100
+			v1 = L3_(v1, p1, p2, p3, 1)
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==1 and v1[p3]==0) or \
+			(v1[p1]==1 and v1[p2]==0 and v1[p3]==1) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==1)): #110
+			v1 = L3_(v1, p1, p2, p3, 2)
+			print("Despues: ", v1)
+
+	elif(L==4): #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		while True:
+			p1 = random.randint(len(v1))
+			p2 = random.randint(len(v1))
+			p3 = random.randint(len(v1))
+			p4 = random.randint(len(v1))
+			if ((p1!=p2) and (p1!=p3) and (p1!=p4) and (p2!=p3) and (p2!=p4) and (p3!=p4)):
+				break
+		print("Antes: ", v1)
+		print("Punto 1: ", p1)
+		print("Punto 2: ", p2)
+		print("Punto 3: ", p3)
+		print("Punto 4: ", p4)
+	 
+		if(v1[p1]==0 and v1[p2]==0 and v1[p3]==0 and v1[p4]==0): #0000
+			print("Despues: ", v1)
+		elif(v1[p1]==1 and v1[p2]==1 and v1[p3]==1 and v1[p4]==1): #1111
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==0 and v1[p3]==0 and v1[p4]==0) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==0 and v1[p4]==0) or \
+			(v1[p1]==0 and v1[p2]==0 and v1[p3]==1 and v1[p4]==0) or \
+			(v1[p1]==0 and v1[p2]==0 and v1[p3]==0 and v1[p4]==1)): #1000
+			v1 = L4_(v1, p1, p2, p3, 1)
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==1 and v1[p3]==0 and v1[p4]==0) or \
+			(v1[p1]==1 and v1[p2]==0 and v1[p3]==1 and v1[p4]==0) or \
+			(v1[p1]==1 and v1[p2]==0 and v1[p3]==0 and v1[p4]==1) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==1 and v1[p4]==0) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==0 and v1[p4]==1) or \
+			(v1[p1]==0 and v1[p2]==0 and v1[p3]==1 and v1[p4]==1)): #1100
+			v1 = L4_(v1, p1, p2, p3, 2)
+			print("Despues: ", v1)
+		elif((v1[p1]==1 and v1[p2]==1 and v1[p3]==1 and v1[p4]==0) or \
+			(v1[p1]==1 and v1[p2]==1 and v1[p3]==0 and v1[p4]==1) or \
+			(v1[p1]==1 and v1[p2]==0 and v1[p3]==1 and v1[p4]==1) or \
+			(v1[p1]==0 and v1[p2]==1 and v1[p3]==1 and v1[p4]==1)): #1110
+			v1 = L4_(v1, p1, p2, p3, 3)
+			print("Despues: ", v1)
+
 def mutacion_heuristica(v1):
 	v1 = np.array([0, 0, 0, 1, 1, 0])
 	L = random.randint(len(v1)-2) #Lambda=Numero de alhelos a cambiar de lugar
